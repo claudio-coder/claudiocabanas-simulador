daa8d45bcabf85815ed93b9d5ee0f66c6fa40d42
Claudio Cabanas <ccabanas@alonenet.com.ar>
2022-09-05 18:55:27 -0300

    aplicando arrays

--- index.js
+++ index.js
@@ -1,44 +1,49 @@
-let turnoCintya = 0; tiempoEsperaCintya = 0;
-let turnoEduardo = 0; tiempoEsperaEduardo = 0;
-let turnoPatricia = 0; tiempoEsperaPatricia = 0;
-let turnoDaniel = 0; tiempoEsperaDaniel = 0;
-let turnoElvira = 0; tiempoEsperaElvira = 0;
-let turnoSilvina = 0; tiempoEsperaSilvina = 0;
-let turnoAlejandra = 0; tiempoEsperaAlejandra = 0;
-let turnoLeonardo = 0; tiempoEsperaLeonardo = 0;
-let turnoSusana = 0; tiempoEsperaSusana = 0;
+const turnosCintya = [];
+const turnosEduardo = [];
+const turnosPatricia = [];
+const turnosDaniel = [];
+const turnosElvira = [];
+const turnosSilvina = [];
+const turnosAlejandra = [];
+const turnosLeonardo = [];
+const turnosSusana = [];
 
 const MAX_TURNOS = 5;
 const TIEMPO_TURNO = 15;
 
 let servicio;
-let nombre;
-// let turno;
-// let tiempoDeServicio;
-// let turnoDeServicio;
+let nombrePaciente;
+let nombreCliente;
 
-function asignarTurno(turnoProfesional, tiempoEspera, nombreProfesional) {
+
+function recibirNombre(texto) {
+    let nombre;
+    do {
+        nombre = prompt(texto);
+    } while (nombre === "")
+
+    return nombre;
+}
+
+function asignarTurno(turnosProfesional, nombreProfesional, servicio) {
     alert(nombreProfesional);
-    if (turnoProfesional < MAX_TURNOS) {
-        do {
-            nombre = prompt("Ingrese el nombre del paciente");
-        } while (nombre === "")
+    if (turnosProfesional.length < MAX_TURNOS) {
 
-        turnoProfesional++;
-        if (turnoProfesional > 1) {
-            tiempoEspera += TIEMPO_TURNO;
-        }
+        nombrePaciente = recibirNombre("Ingrese el nombre del paciente");
+        nombreCliente = recibirNombre("Ingrese el nombre del cliente");
 
-        alert(`Turno #${turnoProfesional} Nombre : ${nombre} ${nombreProfesional} Tiempo de espera: ${tiempoEspera}`);
+        const turno = {
+            nombrePaciente, nombreCliente, servicio
+        };
+
+        turnosProfesional.push(turno);
+
+        alert(`Turno #${turnosProfesional.length} Paciente: ${nombrePaciente} Cliente: ${nombreCliente} ${nombreProfesional} Tiempo de espera: ${TIEMPO_TURNO * (turnosProfesional.length - 1)}`);
     } else {
         alert("se terminaron los turnos")
     }
-
-    return {
-        turnoDeServicio: turnoProfesional,
-        tiempoDeServicio: tiempoEspera,
-    }
 }
+
 
 function solicitarServicioAUsuario(servicio) {
     do {
@@ -53,63 +58,43 @@ function comenzarDiaLaboral() {
 
     switch (servicio) {
         case "1": {
-            const { turnoDeServicio, tiempoDeServicio } = asignarTurno(turnoElvira, tiempoEsperaElvira, "Dra. Elvira")
-            turnoElvira = turnoDeServicio;
-            tiempoEsperaElvira = tiempoDeServicio;
+            asignarTurno(turnosElvira, "Dra. Elvira", "Clínica general")
             break;
         }
         case "2": {
-            const { turnoDeServicio, tiempoDeServicio } = asignarTurno(turnoEduardo, tiempoEsperaEduardo, "Dr. Eduardo")
-            turnoEduardo = turnoDeServicio;
-            tiempoEsperaEduardo = tiempoDeServicio;
+            asignarTurno(turnosEduardo, "Dr. Eduardo", "Cirugía")
             break;
         }
         case "3": {
-            const { turnoDeServicio, tiempoDeServicio } = asignarTurno(turnoPatricia, tiempoEsperaPatricia, "Dra. Patricia")
-            turnoPatricia = turnoDeServicio;
-            tiempoEsperaPatricia = tiempoDeServicio;
+            asignarTurno(turnosPatricia, "Dra. Patricia", "Medicina Holistica")
             break;
         }
         case "4": {
-            const { turnoDeServicio, tiempoDeServicio } = asignarTurno(turnoCintya, tiempoEsperaCintya, "Dra. Cintya")
-            turnoCintya = turnoDeServicio;
-            tiempoEsperaCintya = tiempoDeServicio;
+            asignarTurno(turnosCintya, "Dra. Cintya", "Metodos de Diagnostico")
             break;
         }
         case "5": {
-            const { turnoDeServicio, tiempoDeServicio } = asignarTurno(turnoDaniel, tiempoEsperaDaniel, "Dr. Daniel")
-            turnoDaniel = turnoDeServicio;
-            tiempoEsperaDaniel = tiempoDeServicio;
+            asignarTurno(turnosDaniel, "Dr. Daniel", "Ecografias/Electrocardiogramas")
             break;
         }
         case "6": {
-            const { turnoDeServicio, tiempoDeServicio } = asignarTurno(turnoAlejandra, tiempoEsperaAlejandra, "Dra. Alejandra")
-            turnoAlejandra = turnoDeServicio;
-            tiempoEsperaAlejandra = tiempoDeServicio;
+            asignarTurno(turnosAlejandra, "Dra. Alejandra", "Oncología")
             break;
         }
         case "7": {
-            const { turnoDeServicio, tiempoDeServicio } = asignarTurno(turnoLeonardo, tiempoEsperaLeonardo, "Dr. Leonardo")
-            turnoLeonardo = turnoDeServicio;
-            tiempoEsperaLeonardo = tiempoDeServicio;
+            asignarTurno(turnosLeonardo, "Dr. Leonardo", "Exóticos")
             break;
         }
         case "8": {
-            const { turnoDeServicio, tiempoDeServicio } = asignarTurno(turnoPatricia, tiempoEsperaPatricia, "Dra. Patricia")
-            turnoPatricia = turnoDeServicio;
-            tiempoEsperaPatricia = tiempoDeServicio;
+            asignarTurno(turnosPatricia, "Dra. Patricia", "Fisioterapia")
             break;
         }
         case "9": {
-            const { turnoDeServicio, tiempoDeServicio } = asignarTurno(turnoSilvina, tiempoEsperaSilvina, "Sra. Silvina")
-            turnoSilvina = turnoDeServicio;
-            tiempoEsperaSilvina = tiempoDeServicio;
+            asignarTurno(turnosSilvina, "Sra. Silvina", "Farmacia")
             break;
         }
         case "10": {
-            const { turnoDeServicio, tiempoDeServicio } = asignarTurno(turnoSusana, tiempoEsperaSusana, "Sra. Susana")
-            turnoSusana = turnoDeServicio;
-            tiempoEsperaSusana = tiempoDeServicio;
+            asignarTurno(turnosSusana, "Sra. Susana", "Baños y Peluqueria")
             break;
         }
         default:
@@ -118,11 +103,37 @@ function comenzarDiaLaboral() {
     }
 }
 
+function anularTurno() {
+
+    nombrePaciente = recibirNombre("Ingrese el nombre del paciente");
+    nombreCliente = recibirNombre("Ingrese el nombre del cliente");
+
+    const turnosTotales = [...turnosElvira, ...turnosCintya, ...turnosEduardo, ...turnosAlejandra, ...turnosDaniel, ...turnosLeonardo, ...turnosPatricia, ...turnosSilvina, ...turnosSusana];
+    const turnosReservados = turnosTotales.filter(unTurno => unTurno.nombrePaciente === nombrePaciente && unTurno.nombreCliente === nombreCliente);
+
+    const serviciosTotales = turnosReservados.map(unTurno => `${unTurno.servicio}`);
+    const serviciosNoRepetidos = [...new Set(serviciosTotales)];
+
+    let mensaje = "";
+    serviciosNoRepetidos.forEach((servicio, index) => {
+        mensaje += `${index + 1} - ${servicio} \n`;
+    })
+    reservaAnulada = prompt("Ingrese el ITEM a Anular", mensaje)
+
+
+}
+
 setTimeout(() => {
     do {
-        comenzarDiaLaboral()
-    }
-    while (turnoCintya < MAX_TURNOS || turnoEduardo < MAX_TURNOS || turnoPatricia < MAX_TURNOS || turnoDaniel < MAX_TURNOS
-    || turnoElvira < MAX_TURNOS || turnoSilvina < MAX_TURNOS || turnoAlejandra < MAX_TURNOS || turnoLeonardo < MAX_TURNOS || turnoSusana < MAX_TURNOS)
+        const opcion = prompt("Ingrese 1 para PEDIR TURNO o 2 para ANULAR");
+        if (opcion === "1") {
+            comenzarDiaLaboral();
+        } else if (opcion === "2") {
+            anularTurno();
+        } else {
+            alert("Pusiste algo mal");
+        }
 
-}, 500)
+    }
+    while (true)
+}, 5000)
